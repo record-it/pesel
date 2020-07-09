105b82abdc18b5ba5604b613a75cd56284ab62f9
cs <siwon.cezary@gmail.com>
2020-07-09 22:55:28 +0200

    final version 0.9.1

--- src/main/java/pl/recordit/pesel/Pesel.java
+++ src/main/java/pl/recordit/pesel/Pesel.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020. record-it
+ * Cezary Siwoń
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package pl.recordit.pesel;
 
 import io.vavr.control.Either;
@@ -7,7 +24,6 @@ import java.time.LocalDate;
 
 public interface Pesel {
     Pesel INVALID = InvalidPesel.REF_TO_INVALID;
-    Dupa d = new Dupa();
 
     Option<LocalDate> getBirthDate();
 
--- src/main/java/pl/recordit/pesel/control/Validator.java
+++ src/main/java/pl/recordit/pesel/control/Validator.java
@@ -1,4 +1,21 @@
-package pl.recordit.pesel;
+/*
+ * Copyright (c) 2020. record-it
+ * Cezary Siwoń
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package pl.recordit.pesel.control;
 
 import io.vavr.control.Either;
 import java.util.LinkedList;
@@ -6,7 +23,7 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-class Validator<T, E> {
+public class Validator<T, E> {
 
     static private class Phase<T, E>{
         private final Predicate<T> predicate;
